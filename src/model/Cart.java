cd35e55f7ca83b739d81addba0cfac3ed9f84419
renata <revazgauska@gmail.com>
2022-11-25 15:09:04 -0300

    Cart updates

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,8 +1,13 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
 //    RENATA
 //    ArrayList de produtos
+
+    public static List<Product> cart = new ArrayList<>();
 }
 
 
